3ae6449d4ecdebf4354e046aea60ec215e40b234
Balstar <florentin.rollet@ensiie.fr>
2020-02-01 14:20:17 +0100

    Yes

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -15,7 +15,15 @@ public class PlayerManager: MonoBehaviour
     public float invincibilityTime;
     public float currentinvincibilityTime;
     public bool isInvincible = false;
-
+    public GameObject Heart1;
+    public GameObject Heart2;
+    public GameObject Heart3;
+    public GameObject Brokenheart;
+    private Quaternion zero = new Quaternion(0f,0f,0f,0f);
+    SpriteRenderer sr;
+    public float flickerTime = 3;
+    public float currentflickerTime;
+    private float temp = 0;
     static PlayerManager _instancepm;
 
     public static PlayerManager Instance
@@ -53,9 +61,6 @@ public class PlayerManager: MonoBehaviour
             rb.AddForce(new Vector2(0, JumpSpeed));
             Jumping = true;
         }
-        if (Time.time >= currentinvincibilityTime)
-            isInvincible = false;
-        
 
         Horizontal = Input.GetAxis("Horizontal");
         if (Horizontal < 0)
@@ -63,15 +68,33 @@ public class PlayerManager: MonoBehaviour
         else if (Horizontal > 0)
             spriteRenderer.flipX = false;
         rb.velocity = new Vector2(Horizontal * MoveSpeed,rb.velocity.y);
+
+        if (Time.time >= currentinvincibilityTime)
+            isInvincible = false;
+        if (isInvincible && !GameManager.Instance.isShielded)
+            currentflickerTime = flickerTime + Time.time;
+        if (Time.time <= currentflickerTime)
+        {
+            if (temp < 0.5)
+                temp += Time.deltaTime;
+            else
+            {
+                sr.enabled = !sr.enabled;
+                temp = 0;
+            }
+        }
+        else
+            sr.enabled = true;
+
+        
+            
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag.Equals("Floor") || collision.collider.tag.Equals("Destroyable"))
             Jumping = false;
-    }
-    public void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.tag == "Enemy" && isInvincible == false)
+    
+        if (collision.collider.tag.Equals("Enemy") && isInvincible == false) 
         {
             if ((life -= 1) == 0)
                 Death();
@@ -79,9 +102,23 @@ public class PlayerManager: MonoBehaviour
             {
                 isInvincible = true;
                 currentinvincibilityTime = invincibilityTime + Time.time;
+                switch (life)
+                {
+                    case (2):
+                        Instantiate(Brokenheart, Camera.main.transform.position, zero) ;
+                        Destroy(Heart3);
+                        break;
+                    case (1):
+                        Vector3 posHeart2 = Heart2.transform.position;
+                        Instantiate(Brokenheart, posHeart2, zero);
+                        break;
+                    case (0):
+                        Vector3 posHeart1 = Heart1.transform.position;
+                        Instantiate(Brokenheart, posHeart1, zero);
+                        break;
+                }
             }
         }
-            //Graphics : lost heart
     }
     private void Death()
     {
