9a0a11d1666dd351df7b82600e4538fa1b9c6144
Balstar <florentin.rollet@ensiie.fr>
2020-02-01 11:43:04 +0100

    Power 4 quite good

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,13 +6,40 @@ public class GameManager : MonoBehaviour
 {
 
 
-    private int stage = 3;
+    private int stage = 4;
     GameObject inthevoid = null;
     GameObject[] Allgos;
     bool onMap1 = true;
     BoxCollider2D bc;
     RaycastHit2D hit;
+    public float shieldtime;
+    public float currentshieldtime;
+    public GameObject shield;
+    private bool isShielded = false;
+    public GameObject player;
+    private Quaternion zero = new Quaternion(0f, 0f, 0f, 0f);
     
+
+    static GameManager _instancegm;
+
+    public static GameManager Instance
+
+    {
+        get
+        {
+            if (_instancegm == null)
+            {
+                _instancegm = FindObjectOfType<GameManager>();
+                if (_instancegm == null)
+                {
+                    _instancegm = Instantiate(new GameObject()).AddComponent<GameManager>();
+                }
+            }
+            return _instancegm;
+        }
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,12 +52,14 @@ public class GameManager : MonoBehaviour
         switch (stage)
         {
             case 1:
-                if (Input.GetMouseButtonDown(0))// ce bloc pour le pouvoir du stage 1
+                if (Input.GetMouseButtonDown(0))
+                {// ce bloc pour le pouvoir du stage 1
                     hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-                if (inthevoid == null && hit.collider.gameObject.tag == "Destroyable")
-                {
-                    inthevoid = hit.collider.gameObject;
-                    hit.collider.gameObject.SetActive(false);
+                    if (inthevoid == null && hit.collider.gameObject.tag == "Destroyable")
+                    {
+                        inthevoid = hit.collider.gameObject;
+                        hit.collider.gameObject.SetActive(false);
+                    }
                 }
                 else if (Input.GetMouseButtonDown(0) && inthevoid != null)
                 {
@@ -52,15 +81,36 @@ public class GameManager : MonoBehaviour
                 {
                     foreach (GameObject go in Allgos)
                     {
-                        go.GetComponent<Collider>().isTrigger = !go.GetComponent<Collider>().isTrigger; //à changer pour boxcollider2D
+                        go.GetComponent<Collider2D>().isTrigger = !go.GetComponent<Collider2D>().isTrigger; //à changer pour boxcollider2D
                         // Changer les couleurs pour passer au négatif.
                     }
                 }
                 break;
             case 4:
+                if (Input.GetMouseButtonDown(0) && !isShielded) 
+                {
+                    Instantiate(shield,player.transform.position,zero,player.transform) ; // (?)
+                    PlayerManager.Instance.currentinvincibilityTime = Time.time + PlayerManager.Instance.invincibilityTime ;
+                    PlayerManager.Instance.isInvincible = true;
+                    isShielded = true;
+                }
+                break;
+            default:
                 if (Input.GetMouseButtonDown(0))
-                    ;
-                    break;
+                {
+                    
+                }
+                break;
         }
+        if (isShielded && Time.time >= PlayerManager.Instance.currentinvincibilityTime)
+        {
+            Destroy(shield);
+        }
+            
     }
+    public void Reset()
+    {
+        
+    }
+    
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -10,12 +10,35 @@ public class PlayerManager: MonoBehaviour
     float Horizontal;
     public bool Jumping = false;
     Rigidbody2D rb;
-    private int life = 3;
-    public int invicibilityFrames;
-    
+    public int life = 3;
+    public float invincibilityTime;
+    public float currentinvincibilityTime;
+    public bool isInvincible = false;
+
+    static PlayerManager _instancepm;
+
+    public static PlayerManager Instance
+    {
+        get
+        {
+            if (_instancepm == null)
+            {
+                _instancepm = FindObjectOfType<PlayerManager>();
+                if (_instancepm == null)
+                {
+                    _instancepm = Instantiate(new GameObject()).AddComponent<PlayerManager>();
+                }
+            }
+            return _instancepm;
+        }
+    }
+
 
 
     // Start is called before the first frame update
+
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -28,6 +51,9 @@ public class PlayerManager: MonoBehaviour
             rb.AddForce(new Vector2(0, JumpSpeed));
             Jumping = true;
         }
+        if (Time.time >= currentinvincibilityTime)
+            isInvincible = false;
+        
 
         Horizontal = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(Horizontal * MoveSpeed,rb.velocity.y);
@@ -37,15 +63,22 @@ public class PlayerManager: MonoBehaviour
         if (collision.collider.tag.Equals("Floor") || collision.collider.tag.Equals("Destroyable"))
             Jumping = false;
     }
-    private void OnCollisionEnter(Collision collision)
+    public void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.tag == "Enemy")
+        if (collision.collider.tag == "Enemy" && isInvincible == false)
         {
             if ((life -= 1) == 0)
-                ;
-
-            
+                Death();
+            else
+            {
+                isInvincible = true;
+                currentinvincibilityTime = invincibilityTime + Time.time;
+            }
         }
             //Graphics : lost heart
+    }
+    private void Death()
+    {
+        GameManager.Instance.Reset();
     }
 }
