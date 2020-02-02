879b101c05bf99f7d0b8d8241331e615a6813026
Qwerd71 <57835192+Qwerd71@users.noreply.github.com>
2020-02-02 13:23:37 +0100

    PowBar and CD

--- Assets/Scripts/Jauge_Power.cs
+++ Assets/Scripts/Jauge_Power.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Jauge_Power : MonoBehaviour
 {
 
-    RectTransform scale;
+    private RectTransform scale;
+    public GameObject GameManage;
 
     // Start is called before the first frame update
     void Awake()
@@ -18,18 +19,16 @@ public class Jauge_Power : MonoBehaviour
     {
         
     }
-    public void Start_Fulling()
-    {
-        StartCoroutine(PowerJaugeCoroutine());
-    }
     public IEnumerator PowerJaugeCoroutine()
     {
+        GameManage.GetComponent<GameManager>().End_fill = false;
         scale.localScale = new Vector3(0,scale.localScale.y,scale.localScale.z);
-        for (float t = 5; t < 10; t += Time.deltaTime)
+        for (float t = 5; t < 7; t += Time.deltaTime)
         {
             yield return new WaitForEndOfFrame();
-            scale.localScale = new Vector3(Mathf.InverseLerp(5, 10, t), scale.localScale.y, scale.localScale.z);
+            scale.localScale = new Vector3(Mathf.InverseLerp(5, 7, t), scale.localScale.y, scale.localScale.z);
         }
         scale.localScale = new Vector3(1, scale.localScale.y, scale.localScale.z);
+        GameManage.GetComponent<GameManager>().End_fill = true;
     }
 }
